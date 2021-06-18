7fda57968c182112fcddce6c615918f5aaf5897b
Gareth Whitley <me@garethwhitley.online>
2021-06-18 08:50:08 +0100

    minor changes

--- creditCardCheck.js
+++ creditCardCheck.js
@@ -16,7 +16,7 @@ function getResultArea() {
     return resultArea;
 }
 
-// VARIABLES
+// CONSTANTS
 const cardRegexes = {
     'Visa': /^4[0-9]{12}(?:[0-9]{3})?$/,
     'Mastercard': /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/,
